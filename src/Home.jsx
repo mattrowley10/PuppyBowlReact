47ba1e4618be6628aa290bc406f555197c82377a
Matthew Rowley <mattrowley10@gmail.com>
2023-04-10 19:19:25 -0600

    Trying to fetch and display data

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,5 @@
 import React from 'react'
-import fetch from './fetch'
-
-function PlayerCard(){
-    return <div className='card'>
-        <h2>Player Name</h2>
-        <p>breed</p>
-        <p>email</p>
-        <p>id</p>
-        <button>Details</button>
-    </div>
-}
+import PlayerCard from './PlayerCard'
 
 export default function Home() {
   return (
---
+++ src/PlayerCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+export default function PlayerCard(){
+    
+
+    return <div className='card'>
+        <h2>Player Name</h2>
+        <p>Breed</p>
+        <p>Email</p>
+        <p>Player ID</p>
+        <button>Details</button>
+    </div>
+}
